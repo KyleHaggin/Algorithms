b6a0d5b9381eb7655761ecdac9c60a4e0d35f99f
KyleHaggin <KyleHaggin@users.noreply.github.com>
2020-02-20 15:24:00 -0800

    finish making_change.py with optamization for large input values and general case for denominations

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,8 +3,31 @@
 import sys
 
 
+# def making_change(amount, denominations):
+#     denominations.sort()
+#     num_ways = {}
+#     for denomination in denominations:
+
+
 def making_change(amount, denominations):
-    pass
+    m = len(denominations)
+    # table[i] will be storing the number of solutions for
+    # value i. We need n+1 rows as the table is constructed
+    # in bottom up manner using the base case (n = 0)
+    # Initialize all table values as 0
+    table = [0 for k in range(amount+1)]
+
+    # Base case amount == 0
+    table[0] = 1
+
+    # Pick all coins one by one and update the table[] values
+    # after the index greater than or equal to the value of the
+    # picked coin
+    for i in range(m):
+        for j in range(denominations[i], amount+1):
+            table[j] += table[j-denominations[i]]
+
+    return table[amount]
 
 
 if __name__ == "__main__":
